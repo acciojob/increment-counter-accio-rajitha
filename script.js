18c52b5b4c3bafe4466b954d1fa39d7d9f9c5127
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-30 23:16:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 //your JS code here. If required.
  // Get references to the counter paragraph and the increment button
-  const counterParagraph = document.getElementById('counter');
-  const incrementButton = document.getElementById('incrementBtn');
-
-  // Function to increment the counter value and display an alert
-  function incrementCounter() {
-    // Get the current counter value
-    let currentValue = parseInt(counterParagraph.textContent);
-    // Display an alert with the current value
-    alert('Current Counter Value:  '  + currentValue);
-    // Increment the counter value
-    currentValue++;
-    // Update the counter paragraph with the new value
-    counterParagraph.textContent = currentValue;
-  }
-
-  // Attach click event listener to the increment button
-  incrementButton.addEventListener('click', incrementCounter);
+var incrementcount=document.getElementById("incrementBtn");
+var counterDisplay=document.getElementById("counter");
+var count=0;
+ function incrementCount() {
+ 	count++;
+	 counterDisplay.textContent=count;
+ }
+//Adding eventListner
+incrementBtn.addEventListener("click",incrementCount);
+  
